1eefbafef766b6f7662856d433624caa060ac1b3
OscarFernandoMillanPimentel <o_fernando_m_p@ciencias.unam.mx>
2022-05-23 13:18:57 -0500

    coloresimagen

--- backend/App/views/log_all.php
+++ backend/App/views/log_all.php
@@ -6,7 +6,7 @@
             <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
 
             <a class="navbar-brand m-0" href="/Principal/" target="_blank">
-                <img src="/assets/img/favicon.png" class="navbar-brand-img h-100" alt="main_logo">
+                <img src="/assets/img/foro.png" class="navbar-brand-img h-100" alt="main_logo">
                 <span class="ms-1 font-weight-bold"></span>
                 <p style="margin-top: 15px;"><span class="fa fa-user" style="color: #344767"></span> <?php echo $_SESSION['nombre'];?></p>
             </a>
