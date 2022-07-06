2c9bcebcdc19ec6f5c9a37569e36d4c5cadd5723
OscarFernandoMillanPimentel <o_fernando_m_p@ciencias.unam.mx>
2022-07-06 12:10:27 -0500

    constanciasamhno

--- backend/App/controllers/Constancias.php
+++ backend/App/controllers/Constancias.php
@@ -1046,7 +1046,7 @@ html;
                         <div class="col-auto my-auto">
                             <div class="h-100">
                                 <h5 class="mb-1">
-                                    Constancias AMH
+                                    Constancias ASO CARDIO
                                 </h5>
                                 <p class="mb-0 font-weight-bold text-sm">
                                 </p>
